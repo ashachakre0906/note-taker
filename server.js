d5c17e1e310761e0907cd44c8929c7deeda3d90f
ashachakre0906 <chourpagar.asha@gmail.com>
2022-07-10 12:45:40 -0700

    require() (import) the express module

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const util = require('util');
+
+// Helper method for generating unique ids
+const uuid = require('./helpers/uuid');
+
+const PORT = 3001;
+
+const app = express();
