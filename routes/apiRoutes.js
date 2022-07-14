68f7f1bcc91e171334fe7e64e440118ca0e5f46c
ashachakre0906 <chourpagar.asha@gmail.com>
2022-07-13 19:19:12 -0700

    added get method to retrieve the notes in apiroutes.js

---
+++ routes/apiRoutes.js
@@ -0,0 +1,52 @@
+const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require('../helpers/fsUtils');
+
+// GET Route for retrieving all the notes
+notes.get('/', (req, res) => {
+  readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// DELETE Route for a specific tip
+tips.delete('/:tip_id', (req, res) => {
+  const tipId = req.params.tip_id;
+  readFromFile('./db/tips.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Make a new array of all tips except the one with the ID provided in the URL
+      const result = json.filter((tip) => tip.tip_id !== tipId);
+
+      // Save that array to the filesystem
+      writeToFile('./db/tips.json', result);
+
+      // Respond to the DELETE request
+      res.json(`Item ${tipId} has been deleted 🗑️`);
+    });
+});
+
+// POST Route for a new UX/UI tip
+tips.post('/', (req, res) => {
+  console.log(req.body);
+
+  const { username, topic, tip } = req.body;
+
+  if (req.body) {
+    const newTip = {
+      username,
+      tip,
+      topic,
+      tip_id: uuidv4(),
+    };
+
+    readAndAppend(newTip, './db/tips.json');
+    res.json(`Tip added successfully 🚀`);
+  } else {
+    res.error('Error in adding tip');
+  }
+});
+
+module.exports = tips;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,20 +1,17 @@
 const notes = require ('express').Router();
-const uuid = require('../helpers/uuid.js');
-
-
+const path = require('path');
 
 //GET route for retrieving the notes
 notes.get('/',(req, res) => {
-
-
+res.sendFile(path.join(__dirname,"../public/index.html"))
 
 })
-
-
-
-
-
-
+//GET route for retrieving the notes
+notes.get('/notes',(req, res) => {
+    res.sendFile(path.join(__dirname,"../public/notes.html"))
+    
+    })
+    
 module.exports = notes;
 
 
