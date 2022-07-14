7d7dc1ba143088c43b80a8656b67166c8b7907f7
ashachakre0906 <chourpagar.asha@gmail.com>
2022-07-13 18:56:13 -0700

    created router object

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,20 @@
+const notes = require ('express').Router();
+const uuid = require('../helpers/uuid.js');
+
+
+
+//GET route for retrieving the notes
+notes.get('/',(req, res) => {
+
+
+
+})
+
+
+
+
+
+
+module.exports = notes;
+
+
